1b019202a18d0823948af0d231ae7d73b4cc7461
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-10-05 19:22:39 +0200

    Add class and constructor Category

---
+++ src/main/java/org/example/warehouse/Category.java
@@ -0,0 +1,16 @@
+package org.example.warehouse;
+
+public class Category {
+    private String name;
+
+    private Category(String name) {
+        this.name = name;
+    }
+
+    public static Category of(String name) {
+        return new Category(name);
+    }
+    public String getName() {
+        return name;
+    }
+}
