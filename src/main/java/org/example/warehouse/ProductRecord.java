c273644efe01f15a67f3a108d9f30aaa3ee9420c
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-10-10 16:50:23 +0200

    Add equals and hashCode methods

--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -1,11 +1,21 @@
 package org.example.warehouse;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 
-public record ProductRecord(UUID uuid, String name, Category category, BigDecimal prise) {
+public record ProductRecord (UUID uuid, String name, Category category, BigDecimal price) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProductRecord p)) return false;
+        return Objects.equals(uuid, p.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 
 }
-
-
