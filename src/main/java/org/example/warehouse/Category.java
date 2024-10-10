7290589a3a64cf9786c1a83c55b4effad6581044
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-10-10 16:51:43 +0200

    Add equals and hashCode methods

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -1,14 +1,20 @@
 package org.example.warehouse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Category {
-    private String name;
+    private final String name;
+    private final static Map<String, Category> instances = new HashMap<>();
 
     private Category(String name) {
-        this.name = name;
+        this.name = name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
     public static Category of(String name) {
-        return new Category(name);
+        if (name == null) throw new NullPointerException("Category name cannot be null");
+
+        return instances.computeIfAbsent(name.toLowerCase(), key -> new Category(name));
     }
     public String getName() {
         return name;
