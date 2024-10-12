4e5cbe4a9300280679c57fedd23ff8d48e928d13
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-10-12 13:57:01 +0200

    Update record class

--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -5,17 +5,28 @@ import java.util.Objects;
 import java.util.UUID;
 
 
-public record ProductRecord (UUID uuid, String name, Category category, BigDecimal price) {
+public record ProductRecord(UUID uuid, String name, Category category, BigDecimal price) {
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof ProductRecord p)) return false;
-        return Objects.equals(uuid, p.uuid);
+        if (!(o instanceof ProductRecord that)) return false;
+        return Objects.equals(uuid, that.uuid);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(uuid);
     }
+
+    @Override
+    public String toString() {
+        return "ProductRecord[" +
+                "uuid=" + uuid + ", " +
+                "name=" + name + ", " +
+                "category=" + category + ", " +
+                "price=" + price + ']';
+    }
+
 
 }
